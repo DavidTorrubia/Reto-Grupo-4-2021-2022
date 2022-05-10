e77e97a736ab16b886fbfff00d04954034f1b344
DavidTorrubia <a25585@svalero.com>
2022-05-10 21:28:39 +0200

    añadida animacion al header

--- Content/JS/HeaderScripts.js
+++ Content/JS/HeaderScripts.js
@@ -1,15 +1,15 @@
-var isdeployed = false;
+var Menuisdeployed = false;
 function MenuDeploy () {
     const Boton = document.getElementById("Header-Item-Left-Container-Dropbutton")
     const Desplegable = document.getElementById("Header-Item-Left-Container-Dropbutton-Content")  
-    if (isdeployed == false){
+    if (Menuisdeployed == false){
         Boton.style.backgroundColor="#7f2906";
-        Desplegable.style.display="block";
-        isdeployed = true;
-    } else if (isdeployed == true){
+        Desplegable.classList.add("deployed")
+        Menuisdeployed = true;
+    } else if (Menuisdeployed == true){
         Boton.style.backgroundColor="transparent";
-        Desplegable.style.display="none";
-        isdeployed = false;
+        Desplegable.classList.remove("deployed")
+        Menuisdeployed = false;
     }
     
 }
