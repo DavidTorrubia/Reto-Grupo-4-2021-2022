fb57bf184e70e23dd78cdf059a1dd00c26efab0c
Óscar Ruiz <a25629@svalero.com>
2022-05-19 19:11:38 +0200

    Cambiado implementacion de header y footer de los archivos php

---
+++ Content/HTML/Footer.php
@@ -0,0 +1,10 @@
+<div class="Footer">
+        <div class="Footer-SelectionBar">
+            <a href="AboutUs.php">About Us</a>
+            <a href="">Help</a>
+            <a href="https://mail.google.com/mail/u/1/?pli=1#inbox">Contact Us</a>
+            <a href="AboutUs.php">Work with us</a>
+            <a href="UserPage.php">Settings</a>
+        </div>
+        <p>ecoffe @Copyright</p>
+    </div>
