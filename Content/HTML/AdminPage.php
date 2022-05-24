a1c9f0a2579a22cbe59b486c8f9d816509a8cd89
DavidTorrubia <a25585@svalero.com>
2022-05-24 16:17:35 +0200

    Pequela mod de estilo

--- Content/HTML/AdminPage.php
+++ Content/HTML/AdminPage.php
@@ -8,84 +8,12 @@
     <link rel="stylesheet" href="../CSS/MainStyle.css">
     <link rel="stylesheet" href="../CSS/AdminPageStyle.css">
     <title>Ecoffe - AdminPage</title>
+    <script src="../JS/HeaderScripts.js"></script>
 </head>
 
 
 <body>
-    <div class="Header">
-            <div class="Header-Background"></div>
-            <div class="Header-Item Header-Item-Left-Container">
-                <button type="button" class="Header-Item-Left-Container-Dropbutton"
-                    id="Header-Item-Left-Container-Dropbutton" onclick="MenuDeploy()"><img class="Header-Imagen"
-                        src="../Icons/MenuIcon.png" alt="imagen_menu"></button>
-                <div class="Header-Item-Left-Container-Dropbutton-Content"
-                    id="Header-Item-Left-Container-Dropbutton-Content">
-                    <a href="../HTML/Menu.php">Menu</a>
-                    <a href="../HTML/AboutUs.php">About us</a>
-                </div>
-            </div>
-            <a href="../HTML/LandingPage.php"><img class="Header-Logo" src="../Images/Logo/LogoWhite.png" alt="logo"></a>
-            <div class="Header-Item Header-Item-Right-Container">
-                <div class="Header-Item-Right-Container-UserDropButtonContainer">
-                    <button type="button" class="Header-Item-Right-Container-UserDropButton"
-                        id="Header-Item-Right-Container-UserDropButton" onclick="UserMenuDeploy()"><img
-                            class="Header-Imagen Header-Image-Usericon" src="../Images/defaultUserImage.jpg" /></button>
-                    <div class="Header-Item-Right-Container-UserDropButton-Content"
-                        id="Header-Item-Right-Container-UserDropButton-Content">
-                        <button type="button" class="Header-Item-Right-Container-UserDropButton-Content-Button"
-                            id="Header-Item-Right-Container-UserDropButton-Content-LogIn" onclick="LoginDeploy()">
-                            Login</button>
-                        <div class="Header-Item-Right-Container-UserDropButton-Content-LogInMenu"
-                            id="Header-Item-Right-Container-UserDropButton-Content-LogInMenu">
-                            <form class="Header-Item-Right-Container-UserDropButton-Content-LogInMenu-form">
-                                <h3 class="Header-Item-Right-Container-UserDropButton-Content-LogInMenu-Header">Email</h3>
-                                <input type="text"
-                                    class="Header-Item-Right-Container-UserDropButton-Content-LogInMenu-Input">
-                                <h3 class="Header-Item-Right-Container-UserDropButton-Content-LogInMenu-Header">Password
-                                </h3>
-                                <input type="password"
-                                    class="Header-Item-Right-Container-UserDropButton-Content-LogInMenu-Input">
-                                <input type="submit" value="Log in"
-                                    class="Header-Item-Right-Container-UserDropButton-Content-LogInMenu-Submit">
-                            </form>
-                        </div>
-                        <button type="button" class="Header-Item-Right-Container-UserDropButton-Content-Button"
-                            id="Header-Item-Right-Container-UserDropButton-Content-SignUp" onclick="SignUpDeploy()"> Sign
-                            Up</button>
-                        <div class="Header-Item-Right-Container-UserDropButton-Content-LogInMenu"
-                            id="Header-Item-Right-Container-UserDropButton-Content-SignUpMenu">
-                            <form class="Header-Item-Right-Container-UserDropButton-Content-LogInMenu-form">
-                                <h3 class="Header-Item-Right-Container-UserDropButton-Content-LogInMenu-Header">Email</h3>
-                                <input type="text"
-                                    class="Header-Item-Right-Container-UserDropButton-Content-LogInMenu-Input">
-                                <h3 class="Header-Item-Right-Container-UserDropButton-Content-LogInMenu-Header">Password
-                                </h3>
-                                <input type="password"
-                                    class="Header-Item-Right-Container-UserDropButton-Content-LogInMenu-Input">
-                                <h3 class="Header-Item-Right-Container-UserDropButton-Content-LogInMenu-Header">Repeat
-                                    Password
-                                </h3>
-                                <input type="password"
-                                    class="Header-Item-Right-Container-UserDropButton-Content-LogInMenu-Input">
-                                <input type="submit" value="Sign Up"
-                                    class="Header-Item-Right-Container-UserDropButton-Content-LogInMenu-Submit">
-                            </form>
-                        </div>
-                        <div>
-                            <a type="button" class="Header-Item-Right-Container-UserDropButton-Content-Button UserConfig"
-                                id="Header-Item-Right-Container-UserDropButton-Content-UserConfig "
-                                href="../HTML/UserPage.php">User Config</a>
-                        </div>
-                        <button type="button" class="Header-Item-Right-Container-UserDropButton-Content-Button"
-                            id="Header-Item-Right-Container-UserDropButton-Content-Log off"> Log off</button>
-                    </div>
-                </div>
-                <a href="../HTML/ShoppingCart.php"><img class="Header-Imagen" src="../Icons/ShopppingListWhite.png" /></a>
-                <img class="Header-Imagen Header-Imagen-Bandera" src="../Images/flags/ukflag.png" />
-            </div>
-        </div>
-    </div>
-
+<?php include 'Header.php'?>
     <div class="Content">
         <div class="Content-Buscador">
             <h1 class="Content-Buscador-Text">Buscar:</h1>
