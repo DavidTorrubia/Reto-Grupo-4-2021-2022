4633d7b210eb5e9d640dc1909836ce07027e2c06
DavidTorrubia <a25585@svalero.com>
2022-05-29 19:00:57 +0200

    Modificado script de introduccion de productos al carrito

--- Content/PhpScripts/AddCart.php
+++ Content/PhpScripts/AddCart.php
@@ -3,6 +3,9 @@ if (!isset($_SESSION)) {
     session_start();
     $id = $_GET["id"];
     $quantity = $_GET["quantity"];
+    if ($quantity == null){
+        $quantity = 1;
+    }
     if (!array_key_exists($id, $_SESSION["cart"])) {
         $_SESSION["cart"][$id] = $quantity;
     } else {
